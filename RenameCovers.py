1d80436cd6a88b53ea936e66ede895dcda6aeebe
hervster <hervenyemeck@gmail.com>
2019-06-12 20:31:16 -0400

    Finalized py file.

--- RenameCovers.py
+++ RenameCovers.py
@@ -3,12 +3,47 @@
 # Format: 'Album Name Like This.suffix'
 # Requires The folder name to be the album's name
 
-import os, sys, imghdr
+import os, sys, imghdr, re
 AlbumsPath = os.getcwd() # Stores album directory in folder this file was run in
 dirs = os.listdir(AlbumsPath)
-print(dirs)
+regex1 = '(.*)?(?=(\([A-Za-z]*\))|(?:[\s][-\(]))' # This grabs line upto and not including space followed by - or ( and accounts for words inside parantheses 
+dash = '-'
+paren = '('
+
+i = 0
 for directory in dirs:
-    os.chdir(directory)
-    print(os.getcwd())
-    os.listdir(os.getcwd())
-    os.chdir('..')
+    nL = len(directory)
+    # print(directory[(nL-3):])
+    # if directory[(nL-3):] != '.py' and directory[(nL-3):] != '.7z':  #Checking file extension vs file type, try type check
+    if directory[(nL-3):] not in ('.py', 'txt', '.7z'):
+        os.chdir(AlbumsPath + '\\' + directory)
+        tempDir = os.listdir(os.getcwd())
+        found = False
+        for songFiles in tempDir:
+            sL = len(songFiles)
+            dot = songFiles.index('.')
+            if songFiles[dot:sL] == '.png' or songFiles[dot:sL] == '.jpg':
+                found = True
+                filext = songFiles[dot:sL]
+                if dash in directory or paren in directory:
+                    matchObj = re.search(regex1, directory)
+                    matchInd = matchObj.span()  #span creates tuple of indexes, 
+                    match = directory[matchInd[0]:matchInd[1]]
+                    newName = match + filext
+                    os.rename(songFiles, newName)
+                else:
+                    newName = directory + filext
+                    os.rename(songFiles, newName) # Add if statement that checks for spaces and other characters between dot and filext
+                i += 1
+        if not found:
+            #Write to logfile
+            missing = os.getcwd()
+            os.chdir(AlbumsPath)
+            log = open("Missing Albums.txt", "a+")
+            #if missing not in log:
+            log.write(missing + " is missing an album cover\n")
+            log.close()
+        
+            
+
+#also need call if there is no image file
